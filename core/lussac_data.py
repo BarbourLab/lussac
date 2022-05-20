df46cec034b3f5396ef6cc3ec54b58a591288088
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-05-20 11:39:20 +0200

    Added conversion to µV

--- core/lussac_data.py
+++ core/lussac_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import probeinterface.io
 import spikeinterface.core as si
 import spikeinterface.extractors as se
@@ -26,9 +27,12 @@ class LussacData:
 			The params.json file containing everything we need to know.
 		"""
 
+		self._format_params(params)
 		self.params = params
+
 		self.recording = si.BinaryRecordingExtractor(params['recording']['file'], sampling_frequency=params['recording']['sampling_rate'],
-													 num_chan=params['recording']['n_channels'], dtype=params['recording']['dtype'])
+													 num_chan=params['recording']['n_channels'], dtype=params['recording']['dtype'],
+													 gain_to_uV=params['recording']['gain_uV'], offset_to_uV=params['recording']['offset_uV'])
 		self._setup_probe(params['recording']['probe_file'])
 
 		self.sortings = []
@@ -45,13 +49,28 @@ class LussacData:
 
 		return self.recording.get_sampling_frequency()
 
+	@staticmethod
+	def _format_params(params: dict) -> None:
+		"""
+		Formats the parameters the correct way.
+		For example, is the value is a string containing the path to a file,
+		it will read the file and replace the value with its content.
+
+		@param params: dict
+			A dictionary containing Lussac's parameters.
+		"""
+
+		if isinstance(params['recording']['gain_uV'], str):
+			params['recording']['gain_uV'] = np.load(params['recording']['gain_uV'])
+		if isinstance(params['recording']['offset_uV'], str):
+			params['recording']['offset_uV'] = np.load(params['recording']['offset_uV'])
+
 	def _setup_probe(self, filename: str) -> None:
 		"""
 		Loads the probe geometry into the 'recording' attribute.
 
 		@param filename: str
 			Path to the JSON file containing the probe geometry (ProbeInterface format).
-		@return: None
 		"""
 
 		probe_group = probeinterface.io.read_probeinterface(filename)
