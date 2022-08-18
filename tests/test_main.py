3d92d6633f0ce7f52735a311dc590c0e0b74c6f0
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-08-18 15:45:03 +0200

    Fixed workflow and dataset download

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,17 @@
+import os
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
 import pytest
 import lussac.main
 
 
 params_path = "tests/datasets/cerebellar_cortex/params.json"
+
+if not os.path.exists(params_path):
+	http_response = urlopen("https://zenodo.org/record/7007074/files/lussac2_cerebellar_cortex_dev.zip")
+	zip_file = ZipFile(BytesIO(http_response.read()))
+	zip_file.extractall(path="tests/datasets")
 
 
 def test_parse_arguments():
