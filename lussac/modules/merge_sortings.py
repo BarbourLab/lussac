50895666eaee48e6b5913265473b5305c22d593d
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-10-18 16:03:32 +0200

    Added graph to merge_sortings

--- lussac/modules/merge_sortings.py
+++ lussac/modules/merge_sortings.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numba
+import networkx as nx
 import spikeinterface.core as si
 from lussac.core.module import MultiSortingsModule
 
@@ -10,7 +11,10 @@ class MergeSortings(MultiSortingsModule):
 	"""
 
 	def run(self, params: dict) -> dict[str, si.BaseSorting]:
-		similarity_matrices = self._compute_similarity_matrices(params)
+		# TODO: recenter units between them before comparing them.
+		similarity_matrices = self._compute_similarity_matrices(params['similarity']['TODO'])
+		graph = self._compute_graph(similarity_matrices, params['similarity']['min_similarity'])
+		# TODO: Detect and remove merged units.
 
 		return self.sortings
 
@@ -26,21 +30,26 @@ class MergeSortings(MultiSortingsModule):
 		"""
 
 		similarity_matrices = {}
+		# TODO: remove duplicated spikes for sorting before to_spike_vector().
 		spike_vectors = {name: sorting.to_spike_vector() for name, sorting in self.sortings.items()}
 		n_spikes = {name: np.array([len(sorting.get_unit_spike_train(unit_id)) for unit_id in sorting.unit_ids]) for name, sorting in self.sortings.items()}
 
 		for name1, sorting1 in self.sortings.items():
 			similarity_matrices[name1] = {}
 			for name2, sorting2 in self.sortings.items():
-				if name1 == name2:
+				if name1 == name2 or name2 in similarity_matrices[name1]:
 					continue
+				if name2 not in similarity_matrices:
+					similarity_matrices[name2] = {}
 
 				coincidence_matrix = self._compute_coincidence_matrix(spike_vectors[name1]['sample_ind'], spike_vectors[name1]['unit_ind'],
 																	  spike_vectors[name2]['sample_ind'], spike_vectors[name2]['unit_ind'], max_time)
 
 				similarity_matrix = coincidence_matrix / np.minimum(n_spikes[name1][:, None], n_spikes[name2])
 				expected_matrix = n_spikes[name1][:, None] * n_spikes[name2] * (2*max_time+1) / self.recording.get_num_frames()
-				similarity_matrices[name1][name2] = (similarity_matrix - expected_matrix) / (1 - expected_matrix)
+				corrected_similarity_matrix = (similarity_matrix - expected_matrix) / (1 - expected_matrix)
+				similarity_matrices[name1][name2] = corrected_similarity_matrix
+				similarity_matrices[name2][name1] = corrected_similarity_matrix.T
 
 		return similarity_matrices
 
@@ -79,3 +88,31 @@ class MergeSortings(MultiSortingsModule):
 				coincidence_matrix[spike_labels1[i], spike_labels2[j]] += 1
 
 		return coincidence_matrix
+
+	def _compute_graph(self, similarity_matrices: dict[str, dict[str, np.ndarray]], min_similarity: float) -> nx.Graph:
+		"""
+		Creates a graph containing all the units from all the sortings,
+		and edges between units that are similar.
+
+		@param similarity_matrices: dict[str, dict[str, np.ndarray]]
+			The similarity matrices [sorting1, sorting2, similarity_matrix].
+		@param min_similarity: float
+			The minimal similarity between 2 units to add an edge in the graph.
+		@return graph: nx.Graph
+			The graph containing the edged for each pair of similar units.
+		"""
+
+		graph = nx.Graph()
+		for i, (name1, sorting1) in enumerate(self.sortings.items()):
+			for j, (name2, sorting2) in enumerate(self.sortings.items()):
+				if j <= i:
+					continue
+
+				for i, unit_id1 in enumerate(sorting1.unit_ids):
+					graph.add_node((name1, unit_id1))
+					for j, unit_id2 in enumerate(sorting2.unit_ids):
+						graph.add_node((name2, unit_id2))
+						if similarity := similarity_matrices[name1][name2][i, j] >= min_similarity:
+							graph.add_edge((name1, unit_id1), (name2, unit_id2), similarity=similarity)
+
+		return graph
--- tests/modules/test_merge_sortings.py
+++ tests/modules/test_merge_sortings.py
@@ -20,11 +20,46 @@ def test_compute_coincidence_matrix() -> None:
 	assert coincidence_matrix[1, 0] == 2
 
 
-def test_compute_similarity_matrices(merge_sortings_module: MergeSortings):
+def test_compute_similarity_matrices(merge_sortings_module: MergeSortings) -> None:
 	similarity_matrices = merge_sortings_module._compute_similarity_matrices(6)
 
 	assert 'ks2_low_thresh' in similarity_matrices
 	assert 'ms4_cs' in similarity_matrices['ms3_best']
+	# assert np.max(similarity_matrices['ks2_low_thresh']['ms3_best']) <= 1.0
+
+
+def test_compute_graph(data: LussacData) -> None:
+	sortings = {
+		'1': si.NumpySorting.from_dict({0: np.array([0]), 1: np.array([0]), 2: np.array([0])}, sampling_frequency=30000),
+		'2': si.NumpySorting.from_dict({0: np.array([0]), 1: np.array([0]), 2: np.array([0])}, sampling_frequency=30000),
+		'3': si.NumpySorting.from_dict({0: np.array([0]), 1: np.array([0])}, sampling_frequency=30000)
+	}
+	multi_sortings_data = MultiSortingsData(data, sortings)
+	module = MergeSortings("merge_sortings", multi_sortings_data, "all", "")
+
+	similarity_matrices = {
+		'1': {
+			'2': np.array([[0.9, -0.05, 0.2], [0.02, 0.95, 0.6], [-0.03, 0.0, 0.01]]),
+			'3': np.array([[1.0, 0.03], [0.0, -0.6], [0.0, 0.0]])
+		},
+		'2': {
+			'1': np.array([[0.9, 0.02, -0.03], [-0.05, 0.95, 0.0], [0.2, 0.6, 0.01]]),
+			'3': np.array([[0.95, -0.01], [0.04, -0.04], [0.4, 0.3]])
+		},
+		'3': {
+			'1': np.array([[1.0, 0.0, 0.0], [0.03, -0.6, 0.0]]),
+			'2': np.array([[0.95, 0.04, 0.4], [-0.01, -0.04, 0.3]])
+		}
+	}
+
+	graph = module._compute_graph(similarity_matrices, min_similarity=0.4)
+
+	assert graph.number_of_nodes() == 8
+	assert graph.number_of_edges() == 5
+	assert graph.has_edge(('1', 0), ('2', 0))
+	assert graph.has_edge(('1', 1), ('2', 1))
+	assert graph.has_edge(('2', 2), ('3', 0))
+	assert not graph.has_edge(('1', 2), ('2', 2))
 
 
 @pytest.fixture(scope="function")
