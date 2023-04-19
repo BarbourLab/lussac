813a93d3e98d9fd70f37482c9b29831728e17ed9
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2023-04-19 17:50:36 +0200

    Added test for spike sorting through Lussac

---
+++ tests/core/test_spike_sorting.py
@@ -0,0 +1,27 @@
+import importlib.util
+import pytest
+from lussac.core.lussac_data import LussacData
+from lussac.core.spike_sorting import LussacSpikeSorter
+import spikeinterface.core as si
+
+
+@pytest.mark.skipif(importlib.util.find_spec("hdbscan") is None, reason="Testing the spike sorting requires hdbscan.")
+def test_spike_sorting(data: LussacData) -> None:
+	recording = data.recording.frame_slice(9_000_000, 18_000_000)
+	params = {
+		'sorter': "spykingcircus2",
+		'preprocess': {
+			'filter': {'band': [150., 6000.], 'filter_order': 2, 'ftype': "bessel"},
+			'common_reference': {'operator': "median"}
+		},
+		'sorter_params': {
+			'output_folder': f"{data.tmp_folder}/sc2_ss_test",
+			'apply_preprocessing': True
+		}
+	}
+
+	spike_sorter = LussacSpikeSorter(recording)
+	sorting = spike_sorter.launch(params)
+
+	assert isinstance(sorting, si.BaseSorting)
+	assert sorting.get_num_units() > 0
