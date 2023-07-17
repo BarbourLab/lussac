7d874f6cee7f5ddd27632a0e79e9e3abbb04e0d3
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2023-07-17 11:33:26 +0200

    Make paths relative to fix bug with loading

--- src/lussac/core/pipeline.py
+++ src/lussac/core/pipeline.py
@@ -156,7 +156,7 @@ class LussacPipeline:
 
 		for name, sorting in self.data.sortings.items():
 			path = f"{self.data.logs_folder}/{module_name}/sorting/{name}.pkl"
-			sorting.dump_to_pickle(file_path=path, include_properties=True, relative_to=self.data.logs_folder, recursive=True)
+			sorting.dump_to_pickle(file_path=path, include_properties=True)
 			# TODO: Would be nice to make `relative_to=True` to automatically make it relative to `path.parent`.
 
 	def _load_sortings(self, module_name: str) -> dict[str, si.BaseSorting]:
@@ -171,7 +171,7 @@ class LussacPipeline:
 
 		logging.info("Loading sortings from previous run...\n")
 		sortings_path = glob.glob(f"{self.data.logs_folder}/{module_name}/sorting/*.pkl")
-		sortings = {pathlib.Path(path).stem: si.load_extractor(path, base_folder=self.data.logs_folder) for path in tqdm(sortings_path)}
+		sortings = {pathlib.Path(path).stem: si.load_extractor(path) for path in tqdm(sortings_path)}
 
 		return sortings
 
