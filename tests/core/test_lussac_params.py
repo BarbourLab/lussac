24ce830a2f0f9fcb6a1f14a6de60b8bc10d0a374
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2024-03-20 11:58:20 +0100

    Fix unit tests on Windows

--- tests/core/test_lussac_params.py
+++ tests/core/test_lussac_params.py
@@ -1,7 +1,9 @@
+import pathlib
+
 from lussac.core import LussacParams
 
 
 def test_load_default_params() -> None:
 	params = LussacParams.load_default_params('params_synthetic', '/aze/')
 	print(params)
-	assert params['lussac']['tmp_folder'] == "/aze/lussac/tmp"
+	assert params['lussac']['tmp_folder'] == str(pathlib.Path("/aze") / "lussac" / "tmp")
